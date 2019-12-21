165b2ea1ea7b2a574b9d038ec7999392bdfb82d1
Glitch (hello-express) <none>
2019-12-21 20:17:31 +0000

    🗻📮 Checkpoint ./komutlar/kayıtol.js:8547793/7 ./komutlar/sunucudurum.js:8547793/917 ./bot.js:8547793/8108 ./komutlar/otorol.js:8547793/1800

---
+++ komutlar/sunucudurum.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+exports.run = (client, message, params) => {
+    const xir = new Discord.RichEmbed()
+    .setColor("#0aff00")
+    .setAuthor(message.guild.name, message.guild.iconURL)
+    .addField('Sunucu Adı:', message.guild.name)
+    .addField('Sunucu ID:', message.guild.id)
+    .addField('Kanal sayısı:', message.guild.channels.size)
+    .addField('Sunucu Bölgesi:', message.guild.region)
+    .addField('Üye sayısı:', message.guild.memberCount)
+    .addField('Sahibi:', message.guild.owner)
+    .addField('Oluşturulma tarihi:', message.guild.createdAt)
+    .setThumbnail(message.guild.iconURL);
+    return message.channel.sendEmbed(xir);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['sb'],
+  permLevel: 0
+};
+exports.help = {
+  name: 'sunucubilgi',
+  description: 'Sunucu hakkında bilgi verir.',
+  usage: 'sunucubilgi'
+};
