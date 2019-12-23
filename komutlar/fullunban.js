f073e3104721786e6f7c2f830a97e69499808533
Glitch (hello-express) <none>
2019-12-23 20:11:56 +0000

    🌁💄 Checkpoint ./komutlar/banlananlar.js:8547793/236 ./komutlar/fullunban.js:8547793/713 ./events/ready.js:8547793/5624

---
+++ komutlar/fullunban.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js'); 
+
+exports.run = (client, message, args) => {
+    const tamam = new Discord.RichEmbed()
+        .setColor("GREEN")
+ .setTitle(' Sunucuda ki bütün Yasaklanan kullanıcıların yasağını kaldırdı!')
+    message.channel.sendEmbed(tamam)
+
+  message.guild.fetchBans().then(bans => {
+      bans.forEach(user => {
+        message.guild.unban(user)
+      });
+    });
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 3
+};
+
+exports.help = {
+  name: 'yasaklarıkaldır',
+  description: 'sunucuda ki bütün yasaklı kullanıcıların yasağını kaldırır.',
+  usage: 'yasaklarıkaldır' //salladım düzeltirsiniz
+};
